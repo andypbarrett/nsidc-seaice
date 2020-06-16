78a854e80e54e2bd66650caf875ab8e4eaf8fa3f
Andy Barrett <apbarret@nsidc.org>
2020-06-15 18:45:56 -0600

    add coord grids and reader

---
+++ seaice/nasateam/coord_grids.py
@@ -0,0 +1,27 @@
+import os
+
+import numpy as np
+
+from .constants import NORTH_SHAPE, SOUTH_SHAPE
+
+GRID_COORD_SCALE = 100000.
+
+# Coord Grids were added to project from:
+# ftp://sidads.colorado.edu/pub/DATASETS/seaice/polar-stereo/tools/
+
+mask_dir = os.path.join(os.path.dirname(__file__), 'pkg_data', 'coord_grids')
+NORTH_LAT_GRID_FILENAME = os.path.join(mask_dir, 'psn25lats_v3.dat')
+NORTH_LON_GRID_FILENAME = os.path.join(mask_dir, 'psn25lons_v3.dat')
+SOUTH_LAT_GRID_FILENAME = os.path.join(mask_dir, 'pss25lats_v3.dat')
+SOUTH_LON_GRID_FILENAME = os.path.join(mask_dir, 'pss25lons_v3.dat')
+
+
+NORTH_LAT_GRID = np.fromfile(NORTH_LAT_GRID_FILENAME,
+                             dtype=np.uint32).reshape(*NORTH_SHAPE) / GRID_COORD_SCALE
+NORTH_LON_GRID = np.fromfile(NORTH_LON_GRID_FILENAME,
+                              dtype=np.uint32).reshape(*NORTH_SHAPE) / GRID_COORD_SCALE
+
+SOUTH_LAT_GRID = np.fromfile(SOUTH_LAT_GRID_FILENAME,
+                              dtype=np.uint32).reshape(*SOUTH_SHAPE) / GRID_COORD_SCALE
+SOUTH_LON_GRID = np.fromfile(SOUTH_LON_GRID_FILENAME,
+                              dtype=np.uint32).reshape(*SOUTH_SHAPE) / GRID_COORD_SCALE
